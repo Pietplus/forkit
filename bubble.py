e33553e3f585b17e5135d8b3aa10e6be4c6d4f97
Piet <pemtick@web.de>
2021-03-11 13:42:49 +0100

    Look

---
+++ bubble.py
@@ -0,0 +1,27 @@
+
+# Strings
+
+intro = "This is bubbels, the character of a book i do."
+
+
+bubbles = [                
+
+"                                                   ",
+"       ....                                          ",            
+"     ..    ...  ◊                                  ",                         
+"  ◊  .  • • .  /                                    ",                          
+"   \·    ˘  ..                                       ",                          
+"     .. 	..                                       ",                           
+"      .. ...                                         ",
+"       .. .                                           ",
+"       |  |                                 		   ",
+"       ◊  ◊....._________BUBBELS__________              ",
+"                                  Pietplus                ",
+
+]
+
+
+
+for i in bubbles:
+	print(i)                                            
+
